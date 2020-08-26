9bf6891ac93b21b897c2db6ea4e0c1038f87b39e
William Gathoye <william@gathoye.be>
2020-08-26 14:13:38 +0200

    Switch to Go modules and bump to ESR versions

--- server/plugin.go
+++ server/plugin.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/mattermost/mattermost-server/plugin"
+	"github.com/mattermost/mattermost-server/v5/plugin"
 )
 
 //FileInfo contains file informaton sent to the client
