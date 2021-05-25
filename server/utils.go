a933cbcde58dea1c3fac72783cd406a8ab1253d0
Chetanya Kandhari <chetanya.kandhari@brightscout.com>
2021-05-25 14:36:27 +0530

    update cert verification check

--- server/utils.go
+++ server/utils.go
@@ -54,8 +54,8 @@ func (p *Plugin) GetHTTPClient() *http.Client {
 	config := p.getConfiguration()
 	customTransport := http.DefaultTransport.(*http.Transport).Clone()
 
-	fmt.Println("Disable Verification::", config.DisableCertificateVerification, "!!!!!!!!!!!!!!!!!!!!!!!!!1")
-	if config.DisableCertificateVerification {
+	fmt.Println("Disable Verification::", config.SkipCertificateVerification, "!!!!!!!!!!!!!!!!!!!!!!!!!1")
+	if config.SkipCertificateVerification {
 		customTransport.TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
 	}
 
